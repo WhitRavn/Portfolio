dd3ab506c73ecb10e34aed6bcdeefc4e1cc5360b
Jordy Rocacher <jmr4277@orange.fr>
2023-01-05 19:07:32 +0100

    add content Skill

--- portfolio/src/Components/2Body/AllBoxs/Portfolio.jsx
+++ portfolio/src/Components/2Body/AllBoxs/Portfolio.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
+const img1 = require("../Img/screen1.png");
 export class Portfolio extends React.Component {
   render() {
     return (
       <div className="home-box">
         <div className="home-text">
-          <p>LOOOOOOOOOOOOOOOOOL333333333</p>
+          <img src={img1} />
         </div>
         <div className="home-img"></div>
       </div>
--- portfolio/src/Components/2Body/AllBoxs/Skills.jsx
+++ portfolio/src/Components/2Body/AllBoxs/Skills.jsx
@@ -3,11 +3,68 @@ import React from "react";
 export class Skills extends React.Component {
   render() {
     return (
-      <div className="home-box">
-        <div className="home-text">
-          <p>LOOOOOOOOOOOOOOOOOL22222222222222222222222</p>
+      <div className="skill-section">
+        <div className="skill-box">
+          <div className="skill-title">
+            <span className="span-title">HTML & CSS</span>
+          </div>
+          <div className="skill-bar">
+            <span className="skill-per htmlcss">
+              <span className="tooltip">85%</span>
+            </span>
+          </div>
         </div>
-        <div className="home-img"></div>
+        <div className="skill-box">
+          <div className="skill-title">
+            <span className="span-title">Javascript</span>
+          </div>
+          <div className="skill-bar">
+            <span className="skill-per js">
+              <span className="tooltip">60%</span>
+            </span>
+          </div>
+        </div>
+        <div className="skill-box">
+          <div className="skill-title">
+            <span className="span-title">React JS</span>
+          </div>
+          <div className="skill-bar">
+            <span className="skill-per react">
+              <span className="tooltip">70%</span>
+            </span>
+          </div>
+        </div>
+        <div className="skill-box">
+          <div className="skill-title">
+            <span className="span-title">Node JS</span>
+          </div>
+          <div className="skill-bar">
+            <span className="skill-per node">
+              <span className="tooltip">50%</span>
+            </span>
+          </div>
+        </div>
+        <div className="skill-box">
+          <div className="skill-title">
+            <span className="span-title">PHP</span>
+          </div>
+          <div className="skill-bar">
+            <span className="skill-per php">
+              <span className="tooltip">20%</span>
+            </span>
+          </div>
+        </div>
+        <div className="skill-box">
+          <div className="skill-title">
+            <span className="span-title">MySQL</span>
+          </div>
+          <div className="skill-bar">
+            <span className="skill-per mysql">
+              <span className="tooltip">10%</span>
+            </span>
+          </div>
+        </div>
+        <div className="skill-img"></div>
       </div>
     );
   }
