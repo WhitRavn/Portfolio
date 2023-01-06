0013f74cee395b13962400d132ff874388ea5561
Jordy Rocacher <jmr4277@orange.fr>
2023-01-06 21:03:08 +0100

    add content port and contact

--- portfolio/src/Components/2Body/AllBoxs/Contact.jsx
+++ portfolio/src/Components/2Body/AllBoxs/Contact.jsx
@@ -3,9 +3,13 @@ import React from "react";
 export class Contact extends React.Component {
   render() {
     return (
-      <div className="home-box">
-        <div className="home-text">
-          <p>LOOOOOOOOOOOOOOOOOL4444444444444</p>
+      <div className="contact-section">
+        <div className="contact-container">
+          <h1>Contactez-moi</h1>
+          <span class="material-symbols-outlined" id="mail-icon">
+            mail
+          </span>
+          <p>Mon adresse email</p>
         </div>
         <div className="home-img"></div>
       </div>
--- portfolio/src/Components/2Body/AllBoxs/Portfolio.jsx
+++ portfolio/src/Components/2Body/AllBoxs/Portfolio.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 
-const img1 = require("../Img/screen1.png");
+const imgArray = [
+  {
+    img: require("../Img/screen1.png"),
+    text: "NovalieTravel",
+    class: "port-box one",
+  },
+  {
+    img: require("../Img/screen2.png"),
+    text: "Meute2",
+    class: "port-box two",
+  },
+  {
+    img: require("../Img/screen0.png"),
+    text: "Meander",
+    class: "port-box three",
+  },
+];
+
 export class Portfolio extends React.Component {
+  renderChildrenArray = (item, index) => {
+    return (
+      <div>
+        <div className={item.class} key={index}>
+          <img className="port-img" src={item.img} />
+          <p className="port-text">{item.text}</p>
+        </div>
+      </div>
+    );
+  };
+
   render() {
     return (
-      <div className="home-box">
-        <div className="home-text">
-          <img src={img1} />
-        </div>
-        <div className="home-img"></div>
+      <div className="port-section">
+        <PortRender dataArray={imgArray}>{this.renderChildrenArray}</PortRender>
+      </div>
+    );
+  }
+}
+
+class PortRender extends React.Component {
+  render() {
+    const { dataArray } = this.props;
+    return (
+      <div className="port-container-img">
+        {dataArray.map((item, index) => {
+          return <div key={index}>{this.props.children(item, index)}</div>;
+        })}
       </div>
     );
   }
