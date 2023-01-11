b031a6a7fc74d9ab1f07ab925244875f6b60a1f7
Jordy Rocacher <jmr4277@orange.fr>
2023-01-11 18:19:18 +0100

    context base

--- portfolio/src/Components/2Body/AllBoxs/Home.jsx
+++ portfolio/src/Components/2Body/AllBoxs/Home.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
+
+const theme = {
+  red: {
+    backgroundColor: "red",
+  },
+  green: {
+    backgroundColor: "green",
+  },
+};
+
+const ThemeContext = React.createContext(theme);
+
+function ThemedText({ children }) {
+  const value = useContext(ThemeContext);
+  return (
+    <p style={value} className="h-text-2">
+      {children}
+    </p>
+  );
+}
 
 export class Home extends React.Component {
   render() {
     return (
-      <div className="home-box">
-        <div className="home-text">
-          <p className="h-text-1">Hello, i am </p>
-          <p className="h-text-2">
-            Jordy Rocacher <br />
-            Developpeur Front End
-          </p>
-          <p className="h-text-3">
-            in self taught, I learned web development alone, without training or
-            diploma, only with my motivation and the desire to design and create
-            through programming languages
-          </p>
+      <ThemeContext.Provider value={theme.red}>
+        <div className="home-box">
+          <div className="home-text">
+            <p className="h-text-1">Hello, i am </p>
+            <ThemedText>
+              Jordy Rocacher <br />
+              Developpeur Front End
+            </ThemedText>
+            <p className="h-text-3">
+              in self taught, I learned web development alone, without training
+              or diploma, only with my motivation and the desire to design and
+              create through programming languages
+            </p>
+          </div>
+          <div className="home-img"></div>
         </div>
-        <div className="home-img"></div>
-      </div>
+      </ThemeContext.Provider>
     );
   }
 }
+/*<p className="h-text-2">
+  Jordy Rocacher <br />
+  Developpeur Front End
+</p>;*/
